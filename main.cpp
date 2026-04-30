228ec633143203b713c6e4a45e4f29dfd118e90c
kellyort5 <00186321@uca.edu.sv>
2026-04-30 11:48:56 -0600

    nueva rama

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-int main(){
-    cout << "Hola mundo" << endl; 
-    return 0; 
+int main() {
+    cout << "Hola mundo" << endl;
+    cout << "Nombre: Kelly Ortega" << endl;
+    cout << "Carnet: 00186321" << endl;
+    return 0;
 }
-
